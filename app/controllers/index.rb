502a72311e7d2d7faf784c770dcbbe32546236fe
Alan Alcesto & Zollie Barnes <pair+aalcesto23.zoltron2001@devbootcamp.com>
2016-02-04 16:12:54 -0600

    add ?

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,9 @@
 get '/' do
   wiz = WeatherWizard.new
-  @temp =     wiz.get_weather["currently"]["temperature"]]
+  @temp =     wiz.get_weather["currently"]["temperature"]
   @precip =   wiz.get_weather["currently"]["precipType"]
   @humidity = wiz.get_weather["currently"]["humidity"]
   @wind =     wiz.get_weather["currently"]["windSpeed"]
-
   erb :'index'
 end
 
