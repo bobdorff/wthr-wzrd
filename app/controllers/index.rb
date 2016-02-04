81d7d56e1df04d559f66415e45ee2bd72d8c5f4e
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-04 16:15:19 -0600

    complete backend again

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,6 @@
 get '/' do
   wiz = WeatherWizard.new
-  @temp =     wiz.get_weather["currently"]["temperature"]]
+  @temp =     wiz.get_weather["currently"]["temperature"]
   @precip =   wiz.get_weather["currently"]["precipType"]
   @humidity = wiz.get_weather["currently"]["humidity"]
   @wind =     wiz.get_weather["currently"]["windSpeed"]
