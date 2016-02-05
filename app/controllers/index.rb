ce569712b540b66d3b7c47e70f45771edbea16dc
Alan Alcesto & Zollie Barnes <pair+aalcesto23.zoltron2001@devbootcamp.com>
2016-02-05 09:36:39 -0600

    extract table to partial/build random route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,3 +8,12 @@ get '/' do
   erb :'index'
 end
 
+get '/random' do
+  wiz =WeatherWizard.new
+  data = wiz.rand_weather
+  @temp =     data["currently"]["temperature"]
+  @precip =   data["currently"]["precipType"]
+  @humidity = data["currently"]["humidity"]
+  @wind =     data["currently"]["windSpeed"]
+  erb :'_weather_table.erb', layout: false, locals: {temp: @temp, precip: @precip, humidity: @humidity, wind: @wind}
+end
