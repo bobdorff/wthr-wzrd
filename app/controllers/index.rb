47de37dc3f249dda50814181b05136d464af5246
Alan Alcesto & Zollie Barnes <pair+aalcesto23.zoltron2001@devbootcamp.com>
2016-02-05 13:07:24 -0600

    finishgit add .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -15,20 +15,16 @@ end
 get '/random' do
   wiz = RandomWeatherWizard.new
   data = wiz.get_weather
-  p data
 
   if data && data["flags"]["sources"] != []
     @city = wiz.location_finder["results"][0]["formatted_address"]
-    p @city
     @temp =     data["currently"]["temperature"]
     @precip =   data["currently"]["precipType"]
     @humidity = data["currently"]["humidity"]
     @wind =     data["currently"]["windSpeed"]
     erb :'_weather_table', layout: false, locals: {temp: @temp, precip: @precip, humidity: @humidity, wind: @wind, city:@city}
   else
-        p"************************"
-    p @city = "No data for this location"
-
+    @city = "No data for this location"
     @temp =     "-"
     @precip =   "-"
     @humidity = "-"
