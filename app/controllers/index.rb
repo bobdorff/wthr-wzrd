e1533af1631680f41301bf52ee2e4f16d23ff5bb
Alan Alcesto & Zollie Barnes <pair+aalcesto23.zoltron2001@devbootcamp.com>
2016-02-05 11:15:18 -0600

    complete randomizer

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,10 @@
+require "pp"
+
 get '/' do
   wiz = WeatherWizard.new
   data = wiz.get_weather
+  city = wiz.location_finder["results"][0]["formatted_address"]
+  p city
   @temp =     data["currently"]["temperature"]
   @precip =   data["currently"]["precipType"]
   @humidity = data["currently"]["humidity"]
@@ -9,11 +13,23 @@ get '/' do
 end
 
 get '/random' do
-  wiz =WeatherWizard.new
-  data = wiz.rand_weather
-  @temp =     data["currently"]["temperature"]
-  @precip =   data["currently"]["precipType"]
-  @humidity = data["currently"]["humidity"]
-  @wind =     data["currently"]["windSpeed"]
-  erb :'_weather_table.erb', layout: false, locals: {temp: @temp, precip: @precip, humidity: @humidity, wind: @wind}
+  wiz = RandomWeatherWizard.new
+  data = wiz.get_weather
+  if data != true
+    p"++++++++++++++++++++++"
+    @error = "No data for this location"
+  else
+    city = wiz.location_finder["results"][0]["formatted_address"]
+    @temp =     data["currently"]["temperature"]
+    @precip =   data["currently"]["precipType"]
+    @humidity = data["currently"]["humidity"]
+    @wind =     data["currently"]["windSpeed"]
+    erb :'_weather_table', layout: false, locals: {temp: @temp, precip: @precip, humidity: @humidity, wind: @wind}
+  end
+
+  # p city
+  # p "__________________________________"
+  # p data["currently"]["temperature"]
+  # p "_____________________________________"
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $("#random").on("click", function(event){
+      event.preventDefault();
+      var getRandom = $.ajax({
+        url: "/random",
+        method: "GET"
+        });
+      getRandom.done(function(response){
+        console.log(response);
+        $(".text-container").html(response);
+      });
+    });
 });
